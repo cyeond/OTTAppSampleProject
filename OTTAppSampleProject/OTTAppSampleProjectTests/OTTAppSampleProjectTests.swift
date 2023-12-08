b1fcaa1fcc00ecb1cd3e5884c50a6b25d007dbfe
cyeond <cyeond@gmail.com>
2023-12-08 10:09:18 +0900

    create unit test target

---
+++ OTTAppSampleProject/OTTAppSampleProjectTests/OTTAppSampleProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  OTTAppSampleProjectTests.swift
+//  OTTAppSampleProjectTests
+//
+//  Created by YD on 12/8/23.
+//
+
+import XCTest
+
+final class OTTAppSampleProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
