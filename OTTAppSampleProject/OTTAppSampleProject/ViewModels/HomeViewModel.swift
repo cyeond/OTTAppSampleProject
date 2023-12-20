5bd1cbd956624348ecee093a4ce911206c718fb6
cyeond <cyeond@gmail.com>
2023-12-20 14:03:34 +0900

    ViewModel에서의 API error 대응 추가

--- OTTAppSampleProject/OTTAppSampleProject/ViewModels/HomeViewModel.swift
+++ OTTAppSampleProject/OTTAppSampleProject/ViewModels/HomeViewModel.swift
@@ -10,8 +10,9 @@ import RxSwift
 import RxRelay
 
 class HomeViewModel: ObservableObject {
-    var tvResultsRelay = BehaviorRelay<[APIResult]>(value: [])
-    var movieResultsRelay = BehaviorRelay<[APIResult]>(value: [])
+    var tvResultsRelay = PublishRelay<[APIResult]>()
+    var movieResultsRelay = PublishRelay<[APIResult]>()
+    var apiErrorRelay = PublishRelay<Void>()
     var currentContentType: ContentType = .tv
     
     private let disposeBag = DisposeBag()
@@ -28,10 +29,10 @@ class HomeViewModel: ObservableObject {
     func getTVResults() {
         Observable.combineLatest(API.getData(type: .onTheAirTV), API.getData(type: .airingTodayTV), API.getData(type: .weeklyTrending(.tv)), API.getData(type: .topRated(.tv)), API.getData(type: .popular(.tv)))
             .map { [$0.0, $0.1, $0.2, $0.3, $0.4] }
-            .withUnretained(self)
-            .subscribe(onNext: { weakSelf, result in
+            .subscribe(with: self, onNext: { weakSelf, result in
                 weakSelf.tvResultsRelay.accept(result)
-            }, onError: { error in
+            }, onError: { weakSelf, error in
+                weakSelf.apiErrorRelay.accept(())
             })
             .disposed(by: disposeBag)
     }
@@ -39,10 +40,10 @@ class HomeViewModel: ObservableObject {
     func getMovieResults() {
         Observable.combineLatest(API.getData(type: .upcomingMovies), API.getData(type: .nowPlayingMovies), API.getData(type: .weeklyTrending(.movie)), API.getData(type: .topRated(.movie)), API.getData(type: .popular(.movie)))
             .map { [$0.0, $0.1, $0.2, $0.3, $0.4] }
-            .withUnretained(self)
-            .subscribe(onNext: { weakSelf, result in
+            .subscribe(with: self, onNext: { weakSelf, result in
                 weakSelf.movieResultsRelay.accept(result)
-            }, onError: { error in
+            }, onError: { weakSelf, error in
+                weakSelf.apiErrorRelay.accept(())
             })
             .disposed(by: disposeBag)
     }
